cf48116aa2d27095dd48cc0a6095b150e344b514
Joel Farthing <joel.f.farthing@gmail.com>
2021-07-30 12:07:25 -0600

    init with git, pip, sudoers for joel

--- init.sh
+++ init.sh
@@ -2,6 +2,14 @@
 
 set -eo pipefail
 
+# Allow user 'joel' to use sudo without password
+sudo cat << EOF | (sudo su -c 'EDITOR="tee" visudo -f /etc/sudoers.d/joel')
+# User rules for joel
+joel ALL=(ALL) NOPASSWD:ALL
+EOF
+
+sudo apt-get install -y vim git python3-pip
+
 # Install System Ansible version with python/pip
 python3 -m pip install --upgrade pip
 python3 -m pip install ansible-core
