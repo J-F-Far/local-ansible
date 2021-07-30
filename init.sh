29f509e7b2843ce7237a6082c4c1bc7a13aab8db
Joel Farthing <joel.f.farthing@gmail.com>
2021-07-30 12:18:47 -0600

    minor adjustments to init.sh

--- init.sh
+++ init.sh
@@ -8,9 +8,13 @@ sudo cat << EOF | (sudo su -c 'EDITOR="tee" visudo -f /etc/sudoers.d/joel')
 joel ALL=(ALL) NOPASSWD:ALL
 EOF
 
-sudo apt-get install -y vim git python3-pip
+sudo apt-get install -y git
+sudo apt-get install -y python3-pip
+sudo apt-get install -y vim
 
 # Install System Ansible version with python/pip
+# ( as of writing this script, only Ansible 2.9 is available
+# from Linux Mint 20.2 software center
 python3 -m pip install --upgrade pip
 python3 -m pip install ansible-core
 
